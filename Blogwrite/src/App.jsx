c01a79aa35dc0edd9837ec962b77a9cd0470063a
imSarthakGautam <sarthakgtm175@gmail.com>
2024-09-24 18:02:07 +0545

    Redux configuration done

--- Blogwrite/src/App.jsx
+++ Blogwrite/src/App.jsx
@@ -1,18 +1,50 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import {login , logout} from './store/authSlice'
+import { Header, Footer } from './components'
 
 function App() {
 
-  // console.log(process.env.REACT_APP_APPWRITE_URL);
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  // console.log(process.env.REACT_APP_APPWRITE_URL); --create react app
+  // console.log(import.meta.env.VITE_APPWRITE_URL); -- Vite
+
+ // Loading state
+ const [loading, setLoading] = useState(true)
+ const dispatch = useDispatch()
+
+ useEffect(() => {
+  authService.getCurrentUser()
+  .then((userData)=>{
+
+    if (userData){
+      dispatch(login({userData}))
+    } else {
+      dispatch(logout())
+    }
+  })
+  .finally( ()=> setLoading(false))
   
-  return (
+ }, [])
+
+  
+  return !loading ? (
    <>
-   <div>Hello</div>
+   <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className="w-full block">
+        <Header/>
+        <main>
+          todo
+          {/* outlet: from react router dom */}
+        </main>
+        <Footer/>
+      </div>
+   </div>
    </>
-  )
+  ) : null 
 }
 
 export default App
