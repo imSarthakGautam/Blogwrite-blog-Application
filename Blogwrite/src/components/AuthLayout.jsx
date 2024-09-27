a90411db6af8a7a5b49291a0aded26e5c527e46d
imSarthakGautam <sarthakgtm175@gmail.com>
2024-09-27 21:09:34 +0545

    Creating a protected route component

---
+++ Blogwrite/src/components/AuthLayout.jsx
@@ -0,0 +1,37 @@
+// Creating a protected route component that ensures certain parts of the application (child components) are only accessible based on the user’s authentication status. 
+//If the authentication status does not match the required condition, the user is redirected to a different route (/login or /).
+
+import React, {useEffect, useState} from 'react'
+import { useSelector, UseSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+
+export function Protected({children, authentication= true}) {
+
+    const navigate= useNavigate()
+    const [loader, setLoader] = useState(true)
+    // check in store : userLogin
+    const authStatus= useSelector(state => state.auth.status)
+
+
+    useEffect(() => {
+
+          // if (authStatus ===true){
+        //     navigate("/")
+        // } else if (authStatus === false) {
+        //     navigate("/login")
+        // }
+
+        if (authentication && authStatus !== authentication) {
+            navigate('/login') // if not authenticated, redirect to login
+            
+        } else if (!authentication && authStatus !== authentication){
+            navigate('/')
+        }
+          setLoader(false)
+        
+    }, [authStatus, navigate, authentication])
+
+    return loader ? <h1>Loading...</h1> : <>{children}</>
+
+}
